a32ecdb3a800fc270ad9664cfdc7639df42cb15a
rigs <rigorsyguatbsn@gmail.com>
2023-08-19 17:36:25 +0800

    feat: add randomize effect

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 const gridContainer = document.querySelector('.grid-squares');
 const selectGridButton = document.querySelector('#gridNumber');
 const colorPicker = document.querySelector('#color');
+const randomizeButton = document.querySelector('#randomizeColor');
+const randomizeExit = document.querySelector('#exit');
 
+let isRandomized = false;
 let squares = '';
 
 const gridContainerSize = {
@@ -28,14 +31,35 @@ function createGrid(gridSize) {
         gridContainer.appendChild(breakDiv)
     }
 
-    squares = document.querySelectorAll('.square');
-    squares.forEach((square) => square.addEventListener('mouseenter', () => square.style.backgroundColor = 'blue'));
+    setColor();
+}
+
+function randomizeColor(random = true) {
+    isRandomized = random;
+    setColor();
+    if (random) {
+        console.log('called');
+        randomizeExit.classList.add('active');
+    } else {
+        randomizeExit.classList.remove('active');
+    }
 }
 
 function setColor() {
-    const color = this.value;
+    let color;
     squares = document.querySelectorAll('.square');
-    squares.forEach((square) => square.addEventListener('mouseenter', () => square.style.backgroundColor = color));
+    if (!isRandomized) {
+        color = this.value;
+        squares.forEach((square) => square.addEventListener('mouseenter', () => 
+            square.style.backgroundColor = color === undefined ? 'black' : color
+        ));
+    }
+    else {
+        color = `hsl(${~~(Math.random() * 255)}, 100%, 50%)`;
+        squares.forEach((square) => square.addEventListener('mouseenter', () => 
+            square.style.backgroundColor = color === undefined ? 'black' : color
+        ));
+    }
 }
 
 function setGridNumber() {
@@ -43,7 +67,8 @@ function setGridNumber() {
 
     do {
         numberOfGrids = +prompt('Enter number of grids (1-100): ');
-    } while (numberOfGrids < 1 || numberOfGrids > 100);
+        if (numberOfGrids === 0) return;
+    } while (numberOfGrids > 100 || isNaN(numberOfGrids));
 
     const squares = document.querySelectorAll('.square');
     const breaks = document.querySelectorAll('.break');
@@ -56,3 +81,5 @@ function setGridNumber() {
 
 selectGridButton.addEventListener('click', setGridNumber);
 colorPicker.addEventListener('change', setColor);
+randomizeButton.addEventListener('click', randomizeColor);
+randomizeExit.addEventListener('click', randomizeColor.bind('', false));
