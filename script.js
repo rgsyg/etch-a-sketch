5e69293c3fd538ebb44fe2ef8802bd9fa67a0655
rigs <rigorsyguatbsn@gmail.com>
2023-08-19 13:55:33 +0800

    feat: add function to number of grids refactor: clean codes
    number of grids selection now working

--- script.js
+++ script.js
@@ -1,29 +1,55 @@
 const gridContainer = document.querySelector('.grid-squares');
+const selectGridButton = document.querySelector('#gridNumber');
+
+let squares = '';
+
+let [row, column, gridSize] = [16, 16, 16];
 
 const gridContainerSize = {
     width: gridContainer.offsetWidth,
     height: gridContainer.offsetHeight
 }
 
-for (let i = 0; i < 16; i++) {
-    for (let j = 0; j < 16; j++) {
-        const gridSquare = document.createElement('div');
-        gridSquare.style.width = gridContainerSize.width / 16 + 'px';
-        gridSquare.style.height = gridContainerSize.height / 16 + 'px';
-        gridSquare.style.border = '1px solid black';
-        gridSquare.style.flex = 1;
-        gridSquare.classList.add('square');
-        gridContainer.appendChild(gridSquare);
-    }
-    const breakDiv = document.createElement('div');
-    breakDiv.classList.add('break');
-    gridContainer.appendChild(breakDiv)
-}
+createGrid(16, 16, 16);
 
-const squares = document.querySelectorAll('.square');
+
+function createGrid(row, column, gridSize) {
+    for (let i = 0; i < row; i++) {
+        for (let j = 0; j < column; j++) {
+            const gridSquare = document.createElement('div');
+            gridSquare.style.width = gridContainerSize.width / gridSize + 'px';
+            gridSquare.style.height = gridContainerSize.height / gridSize + 'px';
+            gridSquare.style.border = '1px solid black';
+            gridSquare.style.flex = 1;
+            gridSquare.classList.add('square');
+            gridContainer.appendChild(gridSquare);
+        }
+        const breakDiv = document.createElement('div');
+        breakDiv.classList.add('break');
+        gridContainer.appendChild(breakDiv)
+    }
+    squares = document.querySelectorAll('.square');
+    squares.forEach((square) => square.addEventListener('mouseenter', setColor));
+}
 
 function setColor() {
     this.style.backgroundColor = 'blue';
 }
 
-squares.forEach((square) => square.addEventListener('mouseenter', setColor));
+function setGridNumber() {
+    let numberOfGrids = '';
+
+    do {
+        numberOfGrids = +prompt('Enter number of grids (1-100): ');
+    } while (numberOfGrids < 1 || numberOfGrids > 100);
+
+    const squares = document.querySelectorAll('.square');
+    const breaks = document.querySelectorAll('.break');
+
+    squares.forEach((square) => square.remove());
+    breaks.forEach((divBreak) => divBreak.remove());
+
+    createGrid(numberOfGrids, numberOfGrids, numberOfGrids);
+}
+
+selectGridButton.addEventListener('click', setGridNumber);
